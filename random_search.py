788bda5d369de83db981d023afb019da295d237f
Youngseok Choe <dev.youngseok@gmail.com>
2021-11-13 14:11:57 +0900

    Fix random search

--- random_search.py
+++ random_search.py
@@ -1,30 +1,36 @@
 from tsp_parser import Problem
 from ant_colony import ACO
 import random
+import argparse
+import sys
 
-problem = Problem.from_file("rd100.tsp")
+parser = argparse.ArgumentParser(
+      description = "Hyperparameter Optimisation through Random Search")
 
-best = (4, 144.28, 11.08, 0.5)  # 7542 berlin52
-best_cost = 100000
+parser.add_argument (
+      "tsp_file"
+    , help    = "Path to .tsp file."
+    )
 
-while True:
-    pop_size = max(best[0] + random.choice([1, -1]), 1)
-    alpha = best[1]
-    beta = best[2]
-    rate = best[3]
-    #alpha = best[1] * random.uniform(0.5, 1.5)
-    #beta = best[2] * random.uniform(0.5, 1.5)
-    #rate = max(best[3] + random.choice([0.1, -0.1]), 0)
-    
-    # _, cost_old = ACO(problem, pop_size, best[1], best[2], best[3], problem.dim*100)
-    _, new_cost = ACO(problem, pop_size, alpha, beta, rate, problem.dim*100)
+if __name__ == "__main__":
+    call_args = parser.parse_args()
+    problem = Problem.from_file(call_args.tsp_file)
+    best_cost = sys.maxsize
+    best = []
 
-    print(f"Challenger: {(pop_size, alpha, beta, rate)}, {new_cost}")
-    defended = True
-    if (best_cost >= new_cost):
-        best = (pop_size, alpha, beta, rate)
-        best_cost = new_cost
-        defended = False
+    while True:
+        pop_size = random.randrange(problem.dim) + 1
+        alpha = random.uniform(0, 1000)
+        beta = random.uniform(0, 1000)
+        rate = random.uniform(0, 1)
 
-    
-    print(f"factor:{best[0]}, alpha:{best[1]}, beta:{best[2]}, rate:{best[3]} is best with cost {best_cost} ({'defended' if defended else 'attacked'})")
+        _, new_cost = ACO(problem, pop_size, alpha, beta, rate, problem.dim*100)
+
+        print(f"Challenger: {(pop_size, alpha, beta, rate)}, {new_cost}")
+        if (best_cost > new_cost):
+            best = [(pop_size, alpha, beta, rate)]
+            best_cost = new_cost
+        elif (best_cost == new_cost):
+            best.append((pop_size, alpha, beta, rate))
+        
+        print(f"{best} are best with cost {best_cost}")
